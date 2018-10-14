09dbad10384eaeaa5b67dab0e9f0a2fe53bf9df1
huangyi <hy19841214@126.com>
2018-10-14 16:23:22 +0800

    修改API地址

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://127.0.0.1:8090"',
+  BASE_API: '"http://47.107.68.123:8090"',
 })
