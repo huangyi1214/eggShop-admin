b44c2cdc9682af08cbf478e827cb2fce8c6f7a41
hy19841214@126.com <hy19841214@126.com>
2018-11-04 18:10:49 +0800

    订单

---
+++ src/components/customerDetail.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+    <el-row class="row">
+        <el-col :span="12"><div>名称:<span>{{customer.NAME}}</span></div></el-col>
+        <el-col :span="12"><div>手机号:<span>{{customer.PHONE}}</span></div></el-col>
+    </el-row>
+  
+    <el-row class="row">
+        <el-col :span="12"><div>性别:<span>{{customer.SEX}}</span></div></el-col>
+        <el-col :span="12"><div>年龄:<span>{{customer.AGE}}</span></div></el-col>
+    </el-row>
+        <el-row class="row">
+        <el-col :span="12"><div>房产:<span>{{customer.Property}}</span></div></el-col>
+        <el-col :span="12"><div>汽车情况:<span>{{customer.car}}</span></div></el-col>
+    </el-row>
+    <el-row class="row">
+        <el-col :span="12"><div>户籍:<span>{{customer.census}}</span></div></el-col>
+        <el-col :span="12"><div>社保信息:<span>{{customer.Social_security}}</span></div></el-col>
+    </el-row>
+    <el-row class="row">
+        <el-col :span="12"><div>地址:<span>{{customer.address}}</span></div></el-col>
+        <el-col :span="12"><div>婚姻状况:<span>{{customer.hunyin}}</span></div></el-col>
+    </el-row>
+    <el-row class="row">
+        <el-col :span="12"><div>银行流水:<span>{{customer.bank_flow}}</span></div></el-col>
+        <el-col :span="12"><div>征信状况:<span>{{customer.Credit}}</span></div></el-col>
+    </el-row>
+    <el-row class="row">
+        <el-col :span="12"><div>负债:<span>{{customer.owing}}</span></div></el-col>
+    </el-row>
+  
+
+</div>
+
+  
+</template>
+<script>
+export default {
+    props:{
+        "customer":[Object]
+    }
+}
+</script>
+
+<style>
+.row {
+  margin-bottom: 10px;
+}
+.row div {
+  font-weight: bold;
+}
+.row div span {
+  font-weight: initial;
+}
+</style>
---
+++ src/components/managerDetail.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+      <el-row class="row">
+    <el-col :span="12"><div>名称:<span>{{manage.NAME}}</span></div></el-col>
+    <el-col :span="12"><div>手机号:<span>{{manage.PHONE}}</span></div></el-col>
+  </el-row>
+  <el-row class="row">
+    <el-col :span="12"><div>身份证号码:<span>{{manage.ID_CARD_NO}}</span></div></el-col>
+    <el-col :span="12"><div>身份证有效期:<span>{{manage.VALIDITY}}</span></div></el-col>
+  </el-row>
+  <el-row class="row">
+        <el-col :span="12"><div>信贷类型:<span>{{manage.busnessname}}</span></div></el-col>
+        <el-col :span="12"><div>公司名称:<span>{{manage.COMPANY_NAME}}</span></div></el-col>
+  </el-row>
+  <el-row class="row">
+      <el-col>身份证图片</el-col>
+  </el-row>
+  <el-row :gutter="20" class="row">
+            <el-col :span="8"><div><img v-bind:src=manage.ID_PHOTO style="width:100%"></img></div></el-col>
+            <el-col :span="8"><div><img v-bind:src=manage.ID_PHOTO_Negative style="width:100%"></img></div></el-col>
+  </el-row>
+  <el-row class="row">
+      <el-col>工作证</el-col>
+  </el-row>
+    <el-row>
+            <el-col :span="8"><div><img v-bind:src=manage.WORK_PERMIT style="width:100%"></img></div></el-col>
+  </el-row>
+
+</div>
+
+  
+</template>
+<script>
+export default {
+    props:{
+        "manage":[Object]
+    }
+}
+</script>
+
+<style>
+.row {
+  margin-bottom: 10px;
+}
+.row div {
+  font-weight: bold;
+}
+.row div span {
+  font-weight: initial;
+}
+</style>
